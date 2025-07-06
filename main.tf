6e262746fad8ab180803c83f75c0512e9eb2688e
edgeloopz <208112252+edgeloopz@users.noreply.github.com>
2025-07-05 18:52:46 -0700

    Initial Commit with main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.42"
+    }
+  }
+}
+
+provider "google" {
+  project = "CICDProject"
+  region  = "us-west1"
+}
+
+resource "google_storage_bucket" "project_bucket"{
+    name = "tt-cicd-bucket"
+    location = "US"
+    force_destroy = true
+    public_access_prevention = "enforced"
+
+}
